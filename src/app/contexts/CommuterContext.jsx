e3a16d6798decb82b061ec55c19ad53591c5ac2b
dgtutv <5170192+dgtutv@users.noreply.github.com>
2025-10-04 13:24:27 -0700

    Initial implementation

---
+++ src/app/contexts/CommuterContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useContext } from 'react';
+
+export const CommuterContext = createContext();
+
+export const useCommuter = () => {
+    const context = useContext(CommuterContext);
+    if (!context) {
+        throw new Error('useCommuter must be used within a CommuterProvider');
+    }
+    return context;
+};
