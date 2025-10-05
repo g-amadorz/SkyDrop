f74cbfa34a222c871ae9fc78e15e1cf98c7f61f5
dougwonn <gwonn.dou@gmail.com>
2025-10-05 04:10:43 -0700

    for clekr issue

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <main style={{ minHeight: "100vh", display: "grid", placeItems: "center" }}>
-      <SignUp />
-    </main>
-  );
-}
